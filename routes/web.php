138b39d7687330e43a1c3eec73cb171f0166060b
Diego <diegoserranojaime2001@gmail.com>
2021-03-02 17:21:50 +0100

    Rutas profesor listas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ExamenController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfesorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,10 +29,18 @@ Route::get('/examen/{id}', [ExamenController::class, 'index'])->middleware('auth
 Route::get('/examen/completado/{id}', [ExamenController::class, 'completeTest'])->middleware('auth');
 Route::post('/examen/{id}', [ExamenController::class, 'create'])->middleware('auth');
 
-Route::get('/admin/usuarios',function () {
-    echo 'hola admin';
-})->middleware('admin');
+Route::get('/admin', [AdminController::class, 'index'])->middleware('admin');
 
-Route::get('/alumnos',function () {
-    echo 'hola profesor';
-})->middleware('profesor');
+// Rutas de Admin
+Route::get('/admin/alumnos', [AdminController::class, 'getAlumnos'])->middleware('admin');
+Route::get('/admin/cursos', [AdminController::class, 'getCursos'])->middleware('admin');
+Route::get('/admin/profesores', [AdminController::class, 'getProfesores'])->middleware('admin');
+
+// Rutas de Profesores
+Route::get('/profesor', [ProfesorController::class, 'index'])->middleware('profesor');
+Route::get('/profesor/examenes', [ProfesorController::class, 'getExamenes'])->middleware('profesor');
+Route::get('/profesor/examen/{id}', [ProfesorController::class, 'getExamen'])->middleware('profesor');
+Route::get('/profesor/cursos', [ProfesorController::class, 'getCursos'])->middleware('profesor');
+Route::get('/profesor/curso/{id}', [ProfesorController::class, 'getCurso'])->middleware('profesor');
+Route::get('/profesor/alumno/{id}', [ProfesorController::class, 'getAlumno'])->middleware('profesor');
+Route::get('/profesor/alumno/{id}/examen/{id}', [ProfesorController::class, 'getExamenAlumno'])->middleware('profesor');
